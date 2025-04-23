5935cf36dba5f47987f136e6313c5d2f58dba8d7
reemmohmed <reemmohmedmohmed@gmail.com>
2025-04-23 14:59:22 +0200

    Refactor ProfileViewBody to improve user data display and enhance logout functionality

--- lib/featuers/profile/presentation/view/widgets/profile_view_body.dart
+++ lib/featuers/profile/presentation/view/widgets/profile_view_body.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:our_market/core/function/navigator_push.dart';
 import 'package:our_market/core/function/navigator_to_out.dart';
+import 'package:our_market/core/widgets/app_colors.dart';
 import 'package:our_market/core/widgets/circle_loading.dart';
 import 'package:our_market/core/widgets/titel_text_widget.dart';
 import 'package:our_market/featuers/auth/presentaion/data/user_data_model.dart';
@@ -19,73 +20,80 @@ class ProfileViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     UserDataModel? user = context.read<AuthenticationCubit>().userData;
-    return BlocConsumer<AuthenticationCubit, AuthenticationState>(
-      listener: (context, state) {
-        if (state is SignOutSucsess) {
-          navigatorToOut(context, const LogenView());
-        }
-      },
-      builder: (context, state) {
-        AuthenticationCubit authCubit = context.read<AuthenticationCubit>();
-        return state is SignOutLoading
-            ? const CircleLoading()
-            : Center(
-                child: SingleChildScrollView(
-                  child: Card(
-                    color: Theme.of(context).canvasColor,
-                    child: Column(
-                      children: [
-                        const CircleAvatar(
-                          radius: 55,
-                          child: Text("R"),
-                        ),
-                        TitelTextWidget(text: user?.name ?? "reemMohamed"),
-                        TitelTextWidget(
-                            text: user?.email ?? "reemMohamed@gmail.com"),
-                        const SizedBox(
-                          height: 7,
-                        ),
-                        const Align(
-                          alignment: Alignment.centerLeft,
-                          child: TitelTextWidget(
-                            text: "History",
-                            fontSize: 23,
+    return BlocProvider(
+      create: (context) => AuthenticationCubit()..getUserData(),
+      child: BlocConsumer<AuthenticationCubit, AuthenticationState>(
+        listener: (context, state) {
+          if (state is SignOutSucsess) {
+            navigatorToOut(context, const LogenView());
+          }
+        },
+        builder: (context, state) {
+          AuthenticationCubit authCubit = context.read<AuthenticationCubit>();
+          return state is SignOutLoading || state is GetUserDataLoading
+              ? const CircleLoading()
+              : Center(
+                  child: SingleChildScrollView(
+                    child: Card(
+                      color: Theme.of(context).canvasColor,
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Theme.of(context).primaryColor,
+                            foregroundColor: AppColors.lightScaffold,
+                            radius: 55,
+                            child: TitelTextWidget(
+                                text:
+                                    user?.email.substring(0, 1).toUpperCase() ??
+                                        "?"),
                           ),
-                        ),
-                        const ListHiistory(),
-                        CustomListTitel(
-                          text: "EditName",
-                          icon: Ionicons.person,
-                          onTap: () {
-                            navigatorTo(context, const EditNameView());
-                          },
-                        ),
-                        const SizedBox(
-                          height: 7,
-                        ),
-                        CustomListTitel(
-                          text: "MyOrders",
-                          icon: Icons.shopping_basket_outlined,
-                          onTap: () {
-                            navigatorTo(context, const MyCartView());
-                          },
-                        ),
-                        const SizedBox(
-                          height: 7,
-                        ),
-                        CustomListTitel(
-                          text: "LogOut",
-                          icon: Ionicons.log_out,
-                          onTap: () async {
-                            await authCubit.signOut();
-                          },
-                        ),
-                      ],
+                          TitelTextWidget(text: user?.name ?? "..."),
+                          TitelTextWidget(text: user?.email ?? "..."),
+                          const SizedBox(
+                            height: 7,
+                          ),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: TitelTextWidget(
+                              text: "History",
+                              fontSize: 23,
+                            ),
+                          ),
+                          const ListHiistory(),
+                          CustomListTitel(
+                            text: "EditName",
+                            icon: Ionicons.person,
+                            onTap: () {
+                              navigatorTo(context, const EditNameView());
+                            },
+                          ),
+                          const SizedBox(
+                            height: 7,
+                          ),
+                          CustomListTitel(
+                            text: "MyOrders",
+                            icon: Icons.shopping_basket_outlined,
+                            onTap: () {
+                              navigatorTo(context, const MyCartView());
+                            },
+                          ),
+                          const SizedBox(
+                            height: 7,
+                          ),
+                          CustomListTitel(
+                            text: "LogOut",
+                            icon: Ionicons.log_out,
+                            onTap: () async {
+                              await authCubit.signOut();
+                            },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              );
-      },
+                );
+        },
+      ),
     );
   }
 }
